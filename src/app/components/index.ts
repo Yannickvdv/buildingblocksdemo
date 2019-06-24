f6869dea2f762b834faabef36ff29c616a22e474
Yannick van der Vleuten <yannickvdvleuten@live.nl>
2019-06-24 23:52:08 +0200

    Multiple components made
    - User list started
    - Dashboard started
    - Index.html made prettier
    - Routing started

---
+++ src/app/components/index.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from './app/app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export {
+    AppComponent,
+    UserListComponent,
+    DashboardComponent
+};
