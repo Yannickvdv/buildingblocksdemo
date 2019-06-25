c0b214ff5947775ac1fabd1a2f8455b70f77e3df
Yannick van der Vleuten <yannickvdvleuten@live.nl>
2019-06-25 13:13:44 +0200

    Get and save users to selected user service

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/_services/user.service';
 import { User } from 'src/app/_models/user';
+import { SelectedUsersService } from 'src/app/_services/selected-users.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +11,17 @@ import { User } from 'src/app/_models/user';
 export class DashboardComponent implements OnInit {
   users: User[];
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private selectedUsersService: SelectedUsersService) {}
 
   ngOnInit() {
+    // Once the dashboard is loaded, fill it with default users
     this.userService.getAllUsers().subscribe(users => {
       this.users = users;
     });
+
+    // Listen to any changes in the selected users and apply them
+    this.selectedUsersService.getUsers().subscribe(users => this.users = users);
   }
 }
